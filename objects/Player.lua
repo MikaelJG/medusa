e45b12d1159820461fcff73128b96d03fdfaa3b2
MikaelJG <mjgonsalves@protonmail.com>
2023-01-27 15:28:26 -0500

    two snakes

--- objects/Player.lua
+++ objects/Player.lua
@@ -36,12 +36,17 @@ function Player(debugging)
             love.graphics.polygon(
                 "line",
                 self.x + ((4 / 3) * self.radius) * math.cos(self.angle),
-                self.y -  ((4 / 3) * self.radius) * math.sin(self.angle),
+                self.y - ((4 / 3) * self.radius) * math.sin(self.angle),
                 self.x - self.radius * (2 / 3 * math.cos(self.angle) + math.sin(self.angle)),
                 self.y + self.radius * (2 / 3 * math.sin(self.angle) - math.cos(self.angle)),
                 self.x - self.radius * (2 / 3 * math.cos(self.angle) - math.sin(self.angle)),
                 self.y + self.radius * (2 / 3 * math.sin(self.angle) + math.cos(self.angle))
             )
+
+            love.graphics.setColor(100/255, 230/255, 5/255, 1)
+            love.graphics.circle("line", player.x + 20, player.y - 20, 8)
+            love.graphics.setColor(100/255, 230/255, 5/255, 1)
+            love.graphics.circle("line", player.x - 20, player.y + 20, 8)
         end,
 
         movePlayer = function (self)
