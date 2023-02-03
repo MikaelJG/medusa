2872eeb26060039b602fe4c5afed7fbc30ad5729
MikaelJG <mjgonsalves@protonmail.com>
2023-02-03 09:59:41 -0500

    attack refactor part 1

---
+++ src/effects/attack.lua
@@ -0,0 +1,8 @@
+
+    -- ATTACK
+    attack = {}
+    attack.spriteSheet = love.graphics.newImage("sprites/slash-effect-right.png")
+    attack.grid = anim8.newGrid(16, 16, attack.spriteSheet:getWidth(), attack.spriteSheet:getHeight())
+    attack.animation = {}
+    attack.animation.right = anim8.newAnimation(attack.grid('1-3', 1), 0.2)
+    attack.anim = attack.animation.right
