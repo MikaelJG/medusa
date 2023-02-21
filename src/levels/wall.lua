657ee820ff67705e27a20728c46b9412a90bc535
Julien Le Mee <julemee@gmail.com>
2023-02-21 15:13:29 -0500

    update panels objects

--- src/levels/wall.lua
+++ src/levels/wall.lua
@@ -1,5 +1,4 @@
 -- WALL
-<<<<<<< HEAD
 walls = {}
 if gameMap.layers["walls"] then
   for i, obj in pairs(gameMap.layers["walls"].objects) do
@@ -8,33 +7,3 @@ if gameMap.layers["walls"] then
     table.insert(walls, wall)
   end
 end
-
--- PANELS
-panels = {}
-if gameMap.layers["panels"] then
-  for i, obj in pairs(gameMap.layers["panels"].objects) do
-    local panel = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height) -- (x, y, width, height, mass)
-    panel:setType("static")
-    table.insert(panels, panel)
-  end
-end
-
--- PLANTS
-plants = {}
-if gameMap.layers["plants"] then
-  for i, obj in pairs(gameMap.layers["plants"].objects) do
-    local plant = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height) -- (x, y, width, height, mass)
-    plant:setType("static")
-    table.insert(plants, plant)
-  end
-end
-=======
--- walls = {}
--- if gameMap.layers["walls"] then
---   for i, obj in pairs(gameMap.layers["walls"].objects) do
---     local wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height) -- (x, y, width, height, mass)
---     wall:setType("static")
---     table.insert(walls, wall)
---   end
--- end
->>>>>>> fdb9e203c3e55f76921a23b5b9e14bb76fb1c642
